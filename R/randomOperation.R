2620d7efa2aff62b9b90e2ba950a7bce006ada0b
ThePolywolf <dallin.wolfer@gmail.com>
2026-01-15 15:10:16 -0700

    Random Operation added

---
+++ R/randomOperation.R
@@ -0,0 +1,16 @@
+#' Performs a random operation on the number by a random number between 1 and 1000
+#' @param a an input number
+#' @export
+randomOperation <- function(a) {
+  num <- runif(1, 1, 1000)
+  choice <- sample(1:4, 1)
+
+  ops <- list(
+    function(x) x + num,
+    function(x) x - num,
+    function(x) x * num,
+    function(x) x / num
+  )
+
+  ops[[choice]](a)
+}
